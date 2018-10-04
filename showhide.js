2aec99bfdc9ee46cc636338a3371db636a5c2e67
specialoff <43837773+specialoff@users.noreply.github.com>
2018-10-05 00:43:30 +0200

    yes

---
+++ showhide.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$('#show').click(function()
+{
+	$('#show').css('display','none');
+	$('#data').show();
+	$('#hide').show();
+});
+
+$('#hide').click(function()
+{
+	$('#hide').css('display','none');
+	$('#data').hide();
+	$('#show').show();
+});
